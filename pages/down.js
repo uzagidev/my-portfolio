dd91d0c7711755968f70762ec2f82d1db6d4c116
baodanhit <baodanh1020@gmail.com>
2022-02-19 20:25:44 +0700

    fix error build v2 - add eslint

--- pages/down.js
+++ pages/down.js
@@ -1,8 +1,10 @@
-{
-  /* <div className="text-center">
-              <h1 className="text-3xl font-bold">
-                Hi there! This page is being built, please visit it later.
-              </h1>
-              <h1 className="text-2xl font-bold my-4">❤️ you very much ^^</h1>
-            </div> */
+export default function DownPage(props) {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">
+        Hi there! This page is being built, please visit it later.
+      </h1>
+      <h1 className="text-2xl font-bold my-4">❤️ you very much ^^</h1>
+    </div>
+  );
 }
